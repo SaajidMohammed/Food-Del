d9a1a642accc6a46a08b6e4f608640ba89aac2b4
SaajidMohammed <saajidmohammed6@gmail.com>
2025-08-27 22:47:24 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -7,7 +7,7 @@ const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
-  const url = "http://localhost:4000";
+  const url = "https://food-del-backend-yec4.onrender.com";
 
   // Configure Axios instance
   const api = axios.create({
