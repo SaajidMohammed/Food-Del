be1afd5aa37a2665f0a17d753240b5c84be7053e
SaajidMohammed <saajidmohammed6@gmail.com>
2025-08-27 23:35:07 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,9 @@ const port = process.env.PORT || 4000;
 // This allows the server to parse JSON data in request bodies
 app.use(express.json());
 // This enables Cross-Origin Resource Sharing, allowing your frontend to communicate with this backend
-app.use(cors());
+app.use(cors({
+  origin: 'https://food-del-frontend-5d43.onrender.com/' // Replace with your actual Netlify domain
+}));
 
 
 // --- Database Connection ---
